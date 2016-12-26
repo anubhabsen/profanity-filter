56532d44842ff1844d7e8e2d2040f6e499a4e4f5
Anubhab Sen <anubhabsen@gmail.com>
2016-12-26 17:21:10 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+	if(!isset($_POST['sentence']))
+	{
+		header("Location : index.php");
+	}
+	else
+	{
+		echo $_POST['sentence'];
+	}
+?>
+
+<html>
+    <form method="POST" >
+        <strong><center>Enter sentence to check</cemter></strong>
+        <br>
+        Sentence:
+        <input name="sentence" type="text" id="Sentence">
+        <br>
+        <input type="submit" name="Submit" value="GO">
+    </form>
+</html>

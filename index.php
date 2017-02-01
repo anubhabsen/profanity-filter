f872f63ee25d9488d0512c53b2d5eb874515327b
Anubhab Sen <anubhabsen@gmail.com>
2017-02-02 02:28:37 +0530

    structure html

--- index.php
+++ index.php
@@ -1,19 +1,41 @@
 <html>
-    <form method="POST" action="check.php">
-        <strong><center>Enter text to check</cemter></strong>
-        <br>
-        Text:
-        <input name="sentence" type="text" id="Sentence">
-        <br>
-        <label><input type="radio" name="checktype" id="standard" value="standard">Standard check</label>
-        <br>
-        <label><input type="radio" name="checktype" id="createcustom" value="createcustom">Create Custom check</label>
-        <br>
-        <label><input type="radio" name="checktype" id="rmselect" value="rmselect">Remove Select</label>
-        <br>
-        Custom list:
-        <input name="list" type="text" id="list">
-        <br>
-        <input type="submit" name="Submit" value="GO">
-    </form>
+    <div class="container">
+        <table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#cccccc">
+            <tr>
+                <form method="POST" action="check.php" >
+                    <td>
+                        <table width="100%" border="0">
+                            <tr>
+                                <td colspan="3"><strong><center>Enter the sentence to be checked</cemter></strong></td>
+                            </tr>
+                            <tr>
+                                <td width="78">Sentence</td>
+                                <td width="6">:</td>
+                                <td width="3000"><input name="sentence" type="text" id="Sentence"></td>
+                            </tr>
+                            <tr></tr>
+                            <tr>
+                                <td colspan="3"><label><input type="checkbox" name="checktype" value="standard">Standard check database</label><br></td>
+                            </tr>
+                            <tr>
+                                <td colspan="3"><label><input type="checkbox" name="checktype" value="createcustom">Create Custom database</label><br></td>
+                            </tr>
+                            <tr>
+                                <td colspan="3"><label><input type="checkbox" name="checktype" value="rmselect">Selective removal</label><br></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">Custom list:</td>
+                                <td colspan="3"><input name="list" type="text" id="list"></td>
+                            </tr>
+                            <tr>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td><input type="submit" name="Submit" value="GO"></td>
+                             </tr>
+                        </table>
+                    </td>
+                </form>
+            </tr>
+        </table>
+    </div>
 </html>
